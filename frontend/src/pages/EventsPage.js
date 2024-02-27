41c6d79d8764333f74d875f1f51ccfc1eee9e339
Harshal <akshaydhame2001@gmail.com>
2024-02-27 15:28:55 +0530

    first commit

---
+++ frontend/src/pages/EventsPage.js
@@ -0,0 +1,74 @@
+import EventsList from "../components/EventsList";
+
+import React, { Suspense } from "react";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
+
+const EventsPage = () => {
+  const { events } = useLoaderData();
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
+};
+
+export default EventsPage;
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
+}
+
+// import { Link } from "react-router-dom";
+
+// const DUMMY_EVENTS = [
+//   {
+//     id: "e1",
+//     title: "Some event",
+//   },
+//   {
+//     id: "e2",
+//     title: "Another event",
+//   },
+// ];
+
+// const EventsPage = () => {
+//   return (
+//     <>
+//       <div>EventsPage</div>
+//       <ul>
+//         {DUMMY_EVENTS.map((event) => (
+//           <li key={event.id}>
+//             <Link to={event.id}>{event.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default EventsPage;
